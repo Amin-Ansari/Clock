2f2022e7b22649a4a6dbcf197da0425f09d382c8
Amin Ansari <devansari@yahoo.com>
2022-05-07 23:27:43 +0430

    The minute and the hour work with a bug

--- scripts/index.js
+++ scripts/index.js
@@ -7,9 +7,21 @@ clockWork();
 
 function clockWork() {
   secondMove();
+  minuteMove();
+  hourMove();
 }
 function secondMove() {
   setInterval(function () {
-    times[3].style.transform = `rotateZ(${currentTime.getSeconds() * 10}deg)`;
+    times[3].style.transform = `rotate(${currentTime.getSeconds() * 10}deg)`;
+  }, 1000);
+}
+function minuteMove() {
+  setInterval(function () {
+    times[2].style.transform = `rotate(${currentTime.getMinutes() * 10}deg)`;
+  }, 1000);
+}
+function hourMove() {
+  setInterval(function () {
+    times[1].style.transform = `rotate(${currentTime.getHours() * 10}deg)`;
   }, 1000);
 }
